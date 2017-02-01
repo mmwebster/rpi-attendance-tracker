9deea924723063c5cafa84e46fb1d52ef6237c5c
mmwebster <milo.webster@gmail.com>
2017-01-31 19:34:20 -0800

    Improved card reader test output

--- test-scripts/card-reader.py
+++ test-scripts/card-reader.py
@@ -12,9 +12,15 @@ import evdev # lib for keyboard input event detection
 #######################################################################
 # Main logic
 #######################################################################
+# scan for devices
 devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
-for device in devices:
-    print(device.fn, device.name, device.phys)
+# find card reader device
+if (len(devices) > 0):
+    print("Input event devices available. Devices are: ")
+    for device, i in enumerate(devices):
+        print("   ", i, ") ", device.fn, device.name, device.phys)
+else:
+    print("ERROR: No input event devices available")
 
 #######################################################################
 # House keeping..close interfaces and processes
