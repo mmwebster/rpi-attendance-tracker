cf9301470c6839a56bb71e2338b5bf3195f6b693
mmwebster <milo.webster@gmail.com>
2017-04-04 21:46:55 -0700

    Added support for auto-connection to network

--- StateHandlers.py
+++ StateHandlers.py
@@ -5,11 +5,17 @@
 #################################################################################
 import Jobs
 import time
+import urllib
+from Piezo import Piezo
 from os import environ as ENV
 from LEDIndicator import LEDIndicator
-from Piezo import Piezo
 from pyfsm.StateHandler import StateHandler
 from abc import abstractmethod, abstractproperty, ABCMeta
+
+# all import specifically for production env (not test)
+if not 'ATTENDANCE_TRACKER_TEST' in ENV or \
+   not int(ENV['ATTENDANCE_TRACKER_TEST']) == 1:
+    import wifi
 
 ##########################################################################################
 # FSM state functions
@@ -32,6 +38,27 @@ class InitStateHandler(StateHandler):
             # processing INIT event
             # indicate startup
             args["common_args"]["LEDQueue"].put(LEDIndicator.LED_TYPES[1])
+            # if no in test ENV
+            if not 'ATTENDANCE_TRACKER_TEST' in ENV or \
+               not int(ENV['ATTENDANCE_TRACKER_TEST']) == 1:
+                # attempt to connect to the network if no network connection is present
+                # determine if already connected to a network
+                try:
+                    urllib.urlopen("http://google.com")
+                    print("Already connected to a network")
+                except: urllib.error.URLError as e:
+                    # not already connect to a network, try to connect
+                    # connect to the wifi if no current connection
+                    try:
+                        ret_val = connect_to_wifi(
+                                    args["LocalStorage"].read_config_value('wifi-ssid'),
+                                    args["LocalStorage"].read_config_value('wifi-password')
+                                    )
+                        if ret_val == False:
+                            print("Specified network is out of range")
+                    except:
+                        # either the network is out
+                        print("Specified network password is invalid or another error has occured")
             time.sleep(2)
             return { "next_state": "TEMP", "did_error": False }
         elif (args["event"].name() == "TIMER"):
@@ -75,3 +102,27 @@ class TempStateHandler(StateHandler):
             return { "did_error": False }
         else:
             return { "did_error": True, "error_message": "Invalid event" }
+
+##########################################################################################
+# Utility functions
+##########################################################################################
+def connect_to_wifi(ssid, password):
+    # find all available networks
+    networks = wifi.Cell.all('wlan0')
+    # determine if any ssids match that specified in the config file
+    for network in networks:
+        if network.ssid == configured_ssid:
+            scheme_lookup = wifi.Scheme.find('wlan0', network.ssid)
+            # determine if a scheme has already been created for this ssid
+            if not scheme_lookup == None:
+                print("Found a scheme for network ssid=" + str(network.ssid))
+                return scheme_lookup.activate()
+            else:
+                print("Could not find a scheme for network ssid=" + )
+                new_scheme = wifi.Scheme.for_cell('wlan0', configured_ssid, cell, configured_password)
+                new_scheme.save()
+                return new_scheme.activate()
+
+    # found no matching ssids, print the corresponding error code permanently
+    print("Could not connect to a network, the network with the configured ssid is not in range.")
+    return False
